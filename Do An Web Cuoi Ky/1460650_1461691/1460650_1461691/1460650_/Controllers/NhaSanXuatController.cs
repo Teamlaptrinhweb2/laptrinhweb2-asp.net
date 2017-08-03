421226119a7c7208421f44daa25e78006bb253d8
DELL-PC\DELL <myngoc3456@gmail.com>
2017-08-03 12:10:04 +0700

    Hiển thị partial view nhà sản xuất

---
+++ Do An Web Cuoi Ky/1460650_1461691/1460650_1461691/1460650_/Controllers/NhaSanXuatController.cs
@@ -0,0 +1,91 @@
+﻿using _1460650_.Models.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1460650_.Controllers
+{
+    public class NhaSanXuatController : Controller
+    {
+        // GET: NhaSanXuat
+        public ActionResult Index(int id)
+        {
+            var ds = NhaSanXuatBus.ChiTiet(id);
+            return View(ds);
+        }
+
+        // GET: NhaSanXuat/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: NhaSanXuat/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NhaSanXuat/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NhaSanXuat/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: NhaSanXuat/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NhaSanXuat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: NhaSanXuat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Do An Web Cuoi Ky/1460650_1461691/1460650_1461691/1460650_/Models/Bus/NhaSanXuatBus.cs
@@ -0,0 +1,24 @@
+﻿using DienThoaiShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1460650_.Models.Bus
+{
+    public class NhaSanXuatBus
+    {
+        public static IEnumerable<nhasx> DanhSach()
+        {
+            var db = new DienThoaiShopConnectionDB();
+            return db.Query<nhasx>("select * from nhasx");
+
+        }
+        public static IEnumerable<sanpham> ChiTiet(int id)
+        {
+            var db = new DienThoaiShopConnectionDB();
+            return db.Query<sanpham>("select * from sanpham where NhaSX = '" + id + "'");
+
+        }
+    }
+}
