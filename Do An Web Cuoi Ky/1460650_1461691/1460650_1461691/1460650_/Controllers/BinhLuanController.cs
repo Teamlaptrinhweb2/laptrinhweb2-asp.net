0d444ec3e359b2f1c596f92ba9c34262eb20db65
DELL-PC\DELL <myngoc3456@gmail.com>
2017-08-15 15:34:30 +0700

    Thêm Bình luân sản phẩm

---
+++ Do An Web Cuoi Ky/1460650_1461691/1460650_1461691/1460650_/Controllers/BinhLuanController.cs
@@ -0,0 +1,31 @@
+﻿using _1460650_.Models.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1460650_.Controllers
+{
+    public class BinhLuanController : Controller
+    {
+        [Authorize]
+        // GET: BinhLuan
+        public ActionResult Index(int MaSanPham)
+        {
+            ViewBag.MaSanPham = MaSanPham;
+            return View(BinhLuanBus.DanhSach(MaSanPham));
+        }
+        public ActionResult Create(int MaSanPham = 0, string NoiDung = "")
+        {
+            if (MaSanPham == 0)
+            {
+                return Redirect("/");
+            }
+            BinhLuanBus.Them(MaSanPham, User.Identity.GetUserId(), User.Identity.Name, NoiDung);
+
+            //User.Identity.
+            return RedirectToAction("Details", "SanPham", new { Id = MaSanPham });
+        }
+    }
+}
---
+++ Do An Web Cuoi Ky/1460650_1461691/1460650_1461691/1460650_/Models/Bus/BinhLuanBus.cs
@@ -0,0 +1,30 @@
+﻿using DienThoaiShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1460650_.Models.Bus
+{
+    public class BinhLuanBus
+    {
+        public static void Them(int MaSanPham, string MaTaiKhoan, string TenTaiKhoan, string NoiDung)
+        {
+            using (var db = new DienThoaiShopConnectionDB())
+            {
+                BinhLuan binhluan = new BinhLuan();
+                binhluan.MaSanPham = MaSanPham;
+                binhluan.MaTaiKhoan = MaTaiKhoan;
+                binhluan.NoiDung = NoiDung;
+                db.Execute("INSERT INTO [webbandienthoai].[dbo].[BinhLuan]([MaSanPham],[MaTaiKhoan],[TenTaiKhoan],[NoiDung])VALUES(@0,@1,@2,@3)", MaSanPham, MaTaiKhoan, TenTaiKhoan, NoiDung);
+            }
+        }
+        public static IEnumerable<BinhLuan> DanhSach(int MaSanPham)
+        {
+            using (var db = new DienThoaiShopConnectionDB())
+            {
+                return db.Query<BinhLuan>("select * from BinhLuan where MaSanPham=@0", MaSanPham);
+            }
+        }
+    }
+}
