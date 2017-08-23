c2c4dea17fb174cc11b3abdd6cbb4ab6c95def13
VuThiMyTrinh <cactus.mt@gmail.com>
2017-08-23 15:16:29 +0700

    Tìm kiếm sản phẩm

---
+++ Do An Web Cuoi Ky/1460650_1461691/1460650_1461691/1460650_/Controllers/TimKiemController.cs
@@ -0,0 +1,92 @@
+﻿using _1460650_.Models.Bus;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1460650_.Controllers
+{
+    public class TimKiemController : Controller
+    {
+        // GET: TimKiem
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 5)
+        {
+            var model = TimKiemBus.Search(searchString).ToPagedList(page, pageSize);
+            return View(model);
+        }
+
+        // GET: TimKiem/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TimKiem/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TimKiem/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TimKiem/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TimKiem/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TimKiem/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TimKiem/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Do An Web Cuoi Ky/1460650_1461691/1460650_1461691/1460650_/Models/Bus/TimKiemBus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1460650_.Models.Bus
+{
+    public class TimKiemBus
+    {
+        public static IEnumerable<sanpham> Search(string searchString)
+        {
+            var db = new DienThoaiShopConnectionDB();
+            return db.Query<sanpham>("Select * From sanpham Where TenSanPham like '%" + searchString + "%' or Gia like '%" + searchString + "%' or SoLuongTon like '%" + searchString + "%'");
+        }
+    }
+}
